9df0d4fa8084e702d43a39e7aa20b4ceee234457
Shotaro Takeyama <shotaro@dotinstall.com>
2021-03-16 19:54:13 +0900

    [add]fetchテスト：書籍一覧取得APIをビューに反映（未完成）

---
+++ BookProject/resources/js/all_get.js
@@ -0,0 +1,29 @@
+const url = 'http://homestead-two.test/api/book/all_get';
+
+//操作したいHTML領域を取得
+const title = document.getElementById('title');
+const year_of_issue = document.getElementById('year_of_issue');
+const publisher = document.getElementById('publisher');
+const logic_flag = document.getElementById('logic_flag');
+
+//APIからJSONデータを取得する
+fetch(url)
+   .then(response => {
+       return response.json(); //ここでBodyからJSONを返す
+   })
+   .then(result => {
+       Example(result);  //取得したJSONデータを関数に渡す
+   })
+   .catch(e => {
+       console.log(e);  //エラーをキャッチし表示
+})
+//JSONデータを引数に受け取ってDOM操作を行う関数を作成
+function Example(jsonObj){
+  console.log(jsonObj);
+  const data = jsonObj.data;
+  title.textContent = data[0].title;
+  year_of_issue.textContent = '発行年： ' + data[0].year_of_issue + '年';
+  publisher.textContent = '出版社： ' + data[0].publisher
+
+  
+}
--- BookProject/resources/views/list_of_books.blade.php
+++ BookProject/resources/views/list_of_books.blade.php
@@ -7,9 +7,9 @@
 
 <main>
 <!-- バリューはkeywordの変数 -->
-<!-- <form action="http://54.248.141.223/list_of_books" method="GET">
+<form action="http://54.248.141.223/list_of_books" method="GET">
   <div id="search">
-    <input id="sbox" name="s" type="text" value=keyword placeholder="キーワードを入力" />
+    <input id="sbox" name="s" type="text" value="" placeholder="キーワードを入力" />
   
   <div class="dropdown">
     <button type="button" id="dropdown" id="search"
@@ -35,28 +35,27 @@
     並び替え
     </button>
     <ul class="dropdown-menu">
-      <li><button class="dropdown-item" value="発行年">発行年</button></li>
-      <li><button class="dropdown-item" value="著者">著者</button></li>
       <li><button class="dropdown-item" value="タイトル">タイトル</button></li>
+      <li><button class="dropdown-item" value="発行年">発行年</button></li>
     </ul>
   </div>
   
   <input id="sbtn" id="search" type="submit" value="検索" /></div>
-  </form> -->
+  </form>
 
 
   <div id="book_p">
     <div id="book">
-      <img src="" id="image" alt="表紙画像" width="135" height="135" />
+      <img src="" id="cover_pic" alt="表紙画像" width="135" height="135" />
     </div>
     
     <div id="book">
       <div id="text">
         <p id="category">カテゴリ</p>
-        <p id="title">タイトル：</p>
-        <p>発行年：</p>
-        <p>出版社：</p>
-          <p>貸出状況：貸出可</p>
+        <p id="title"></p>
+        <p id="year_of_issue"></p>
+        <p id="publisher"></p>
+        <p id="logic_flag">貸出状況：貸出可</p>
       </div>
     </div>
   </div>
@@ -110,5 +109,6 @@
       </button>
     </div>
   </div>
+<script src="{{ mix('js/all_get.js') }}"></script>  
 </main>
 @endsection
